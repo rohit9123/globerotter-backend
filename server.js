f1300bd8d7e8741079eb0abfc9cc04de8b8b7935
manoj-compro <60548779+rohit9123@users.noreply.github.com>
2025-03-02 22:20:14 +0530

    minor update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "http://localhost:3000", // Replace with your frontend URL
+  origin: "https://globetrotter-frontend-puce.vercel.app/", // Replace with your frontend URL
   credentials: true
 }));
 
